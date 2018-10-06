3a1e9cf8b3e9b46e6075c8216bdfa4797a1deba7
David Burela <David.Burela@gmail.com>
2018-10-05 22:14:27 -0700

    adding advanced tests

--- truffle.js
+++ truffle.js
@@ -1,4 +1,11 @@
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
+
+  mocha: {
+    reporter: "mocha-junit-reporter",
+    reporterOptions: {
+      mochaFile: 'TEST-truffle.xml'
+    }
+  }
 };
